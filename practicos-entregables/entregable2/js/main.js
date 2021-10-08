a881bba1d96bc685a8b7d79efd559f966f605ceb
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2021-10-08 19:44:19 -0300

    little error

--- practicos-entregables/entregable2/js/main.js
+++ practicos-entregables/entregable2/js/main.js
@@ -206,7 +206,7 @@ function timer(stop){
     minutes = document.getElementById("minutos");
 
     window.setInterval( () => {
-        if(stop != countSeconds){
+        if(stop != countMinutes){
             if (countSeconds == 60){
                 countSeconds = 0;
                 countMinutes++;
