abdf77b334de6e363a2730fe61c935dd242dc140
Rosario Tani Gomez <ro.tani.gomez@gmail.com>
2021-10-15 18:41:35 -0300

    Presskey

--- practicos-entregables/entregable3/js/main.js
+++ practicos-entregables/entregable3/js/main.js
@@ -1,2 +1,15 @@
 "use strict";
+let keyDown= false ;
+let persona = document.getElementById("person");
+window.addEventListener("keydown",(e)=>{
+keyDown = true;
+ if (e.keyCode == '38'){
+     console.log('hola')
+   persona.className= "person";
+ }
+ if (e.keyCode == '40'){
+    persona.style.animation= "walk 1s steps(7) infinite";
+ }
+
+});
 
