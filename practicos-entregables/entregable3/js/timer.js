070fb76e088e2ef8b244eeed401d9a40fc557708
Rosario Tani Gomez <ro.tani.gomez@gmail.com>
2021-10-15 19:15:43 -0300

    move class

---
+++ practicos-entregables/entregable3/js/timer.js
@@ -0,0 +1,26 @@
+class timer {
+    static start_timer (){
+            let stop = 1
+            let countSeconds = 0;
+            let countMinutes = 0;
+            // seconds = document.getElementById("segundos");
+            // minutes = document.getElementById("minutos");
+        
+            window.setInterval( () => {
+                if(stop != countMinutes){
+                    if (countSeconds == 60){
+                        countSeconds = 0;
+                        countMinutes++;
+                        // minutes.innerHTML = countMinutes;
+                        if(countMinutes == 0){
+                            countMinutes = 0;
+                        }
+                    }
+                // seconds.innerHTML = countSeconds;
+                countSeconds++;
+                }else{
+                    console.log("fin")
+                }
+            }, 1000)
+        }
+    }
