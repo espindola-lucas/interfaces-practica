2de478157bc176b56b6f734f00aba3f2983f8846
Rosario Tani Gomez <ro.tani.gomez@gmail.com>
2021-10-19 20:34:09 -0300

    down

--- practicos-entregables/entregable3/js/CrashObjects.js
+++ practicos-entregables/entregable3/js/CrashObjects.js
@@ -7,26 +7,26 @@ class CrashObjects{
     }
     
     
-    static  randomColosion ($selected){
+    static  randomColosion (selected){
     // let elegido= Help.getRandomInt(0,3)
     let flor = document.getElementById("flor");
     let fuego = document.getElementById("fuego");
     let flor1 = document.getElementById("flor1");
     let fuego1 = document.getElementById("fuego1");
-        switch($selected){  
-            case 0: 
+        switch(selected){  
+            case 1: 
             console.log('adentro');
                 flor.className= "flor";
                 break;
     
-            case 1 : 
+            case 2 : 
             fuego.className= "fuego";
             console.log ('fuego')
                 break;
-            case 2 :
+            case 3 :
                 fuego1.className= "fuego";
                 break;
-            case 3 :
+            case 4 :
                 flor1.className= "flor";
                 break;
     
--- practicos-entregables/entregable3/js/Person.js
+++ practicos-entregables/entregable3/js/Person.js
@@ -4,7 +4,7 @@ class Person {
 
   static CargarPerson (){
     let persona = document.getElementById("person");
-    persona.style.background = 'url('+Persona.img+')';
+    persona.style.background = 'url('+Persona.imgR+')';
     persona.className= "person";
   }
 
@@ -12,10 +12,26 @@ class Person {
     let persona = document.getElementById("person");
     console.log(e.keyCode);
     if (e.keyCode == '38'){
+      persona.style.background = 'url('+Persona.imgU+')';
         persona.className= "personUp";
     }
     if (e.keyCode == '40'){
-        persona.className= "perso";
+      persona.style.background = 'url('+Persona.imgD+')';
+      persona.className= "personD";
     }
   }
+  
+  static  up_key (e) {
+    let persona = document.getElementById("person");
+    console.log(e.keyCode);
+    if (e.keyCode == '38'){
+      persona.style.background = 'url('+Persona.imgR+')';
+      persona.className = "person"; 
+    }
+    if (e.keyCode == '40'){
+      persona.style.background = 'url('+Persona.imgR+')';
+      persona.className = "person"; 
+    }
+  }
+  
 }
