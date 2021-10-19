4280b411d3b67add40364c332d920d89e036f568
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2021-10-19 09:37:42 -0300

    Borro archivos innecesarios

--- practicos-entregables/pruebas/script.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-window.addEventListener('load', () => {
-    const container_load = document.querySelector('.conteiner_load');
-    container_load.style.opacity = 0;
-    container_load.style.visiblity = 'hidden';
-});
