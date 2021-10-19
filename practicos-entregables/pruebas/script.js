44fd34b27b63fa85d88947b90233b8a9ae5038b5
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2021-10-19 09:35:57 -0300

    I add menu at homepage

---
+++ practicos-entregables/pruebas/script.js
@@ -0,0 +1,6 @@
+"use strict";
+window.addEventListener('load', () => {
+    const container_load = document.querySelector('.conteiner_load');
+    container_load.style.opacity = 0;
+    container_load.style.visiblity = 'hidden';
+});
