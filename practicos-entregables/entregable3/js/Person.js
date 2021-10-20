7c90708d3e7d80f60a56091c2f5a8bc7c94ce6af
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2021-10-20 19:09:48 -0300

    add keyframes

--- practicos-entregables/entregable3/js/Person.js
+++ practicos-entregables/entregable3/js/Person.js
@@ -14,10 +14,12 @@ class Person {
     if (e.keyCode == '38'){
       persona.style.background = 'url('+Persona.imgU+')';
         persona.className= "personUp";
+        // persona.style.background = 'url(images/ninjaDead.png)';
+        // persona.className = 'personDead';
     }
     if (e.keyCode == '40'){
       persona.style.background = 'url('+Persona.imgD+')';
-      persona.className= "personD";
+      persona.className= "personDown";
     }
   }
   
