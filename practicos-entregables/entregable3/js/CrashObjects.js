608d348d5eaae3c27f3e50e3bc3af5c5583e6e46
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2021-10-21 14:26:59 -0300

    adjusting

--- practicos-entregables/entregable3/js/CrashObjects.js
+++ practicos-entregables/entregable3/js/CrashObjects.js
@@ -2,31 +2,26 @@
 
 class CrashObjects{
 
-    static  terminarAnimacion (){
+    static terminarAnimacion (){
         console.log(termino);
     }
 
-    static  randomColosion (selected){
+    static randomColosion (selected){
     // let elegido= Help.getRandomInt(0,3)
     let flor = document.getElementById("flor");
     let fuego = document.getElementById("fuego");
     let moneda = document.getElementById("moneda");
-    // let estrella = document.getElementById("estrella");
         switch(selected){  
             case 1: 
-            console.log('adentro');
+                console.log('adentro');
                 flor.className= "flor";
                 break;
-    
-            case 4: 
-            fuego.className= "fuego";
+            case 2: 
+                fuego.className= "fuego";
             break;
-            // case 3 :
-            //     estrella.className= "estrella";
-            //     break;
-            case 2:
+            case 3:
                 moneda.className= "moneda";
                 break;
-            }
         }
     }
+}
--- practicos-entregables/entregable3/js/main.js
+++ practicos-entregables/entregable3/js/main.js
@@ -1,7 +1,7 @@
 "use strict";
+
 let i = 1;
-let divHills = document.getElementById("hills");
-let divTree =  document.getElementById("tree");
+
 const Persona ={
     imgR:"images/ninjaRunning.png",
     imgU:"images/ninjaUp.png",
@@ -16,34 +16,25 @@ const Fondo ={
 let flor = document.getElementById("flor");
 flor.addEventListener("animationend", function () {
     flor.className = ""; 
-    i = 2;
+    i = 1;
     CrashObjects.randomColosion(i);
 });
 
 let moneda = document.getElementById("moneda");
 moneda.addEventListener("animationend", function () {
     moneda.className = ""; 
-    i = 3;
+    i = 2;
     CrashObjects.randomColosion(i);
 });
 
 let fuego = document.getElementById("fuego");
 fuego.addEventListener("animationend", function () {
     fuego.className = ""; 
-    i = 1;
+    i = 3;
     CrashObjects.randomColosion(i);
 });
 
-// let estrella = document.getElementById("estrella");
-// estrella.addEventListener("animationend", function () {
-//     estrella.className = ""; 
-//     i = 4;
-//     CrashObjects.randomColosion(i);
-// });
-
 function  initGame() {
-    // divHills.style.background = 'url('+Fondo.hills+')repeat-x';
-    // divTree.style.background = 'url('+Fondo.tree+')repeat-x';
     Person.CargarPerson();
     // timer.start_timer ();
     window.addEventListener("keydown",Person.press_key); 
@@ -51,10 +42,11 @@ function  initGame() {
 }
 
 window.setInterval( () => {
-CrashObjects.randomColosion(i);
-},4000);
-window.setInterval( () => {
-    CrashObjects.randomColosion(3);
-},16000);
+    CrashObjects.randomColosion(i);
+}, 4000);
+
+// window.setInterval( () => {
+//     CrashObjects.randomColosion(3);
+// }, 16000);
 
 document.addEventListener("DOMContentLoaded", initGame());
