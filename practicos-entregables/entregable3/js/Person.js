c6c68adb4f25157b5655de1b192f5b59b569f75b
Rosario Tani Gomez <ro.tani.gomez@gmail.com>
2021-10-22 19:40:03 -0300

    Colisiones arregladas

--- practicos-entregables/entregable3/js/Person.js
+++ practicos-entregables/entregable3/js/Person.js
@@ -4,13 +4,13 @@ class Person {
 
     static CargarPerson (){
         let persona = document.getElementById("person");
-        persona.style.background = 'url('+Persona.imgR+')';
+        // persona.style.background = 'url('+Persona.imgR+')';
         persona.className= "person";
     }
 
     static  press_key (e) {
         let persona = document.getElementById("person");
-        console.log(e.keyCode);
+        
         if (e.keyCode == '38'){
             persona.style.background = 'url('+Persona.imgU+')';
             persona.className= "personUp";
@@ -25,7 +25,6 @@ class Person {
 
     static  up_key (e) {
         let persona = document.getElementById("person");
-        console.log(e.keyCode);
         if (e.keyCode == '38'){
             persona.style.background = 'url('+Persona.imgR+')';
             persona.className = "person"; 
