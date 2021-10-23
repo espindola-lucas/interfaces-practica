932142997a95b917d21825f1668bd4443434cfa3
Rosario Tani Gomez <ro.tani.gomez@gmail.com>
2021-10-23 18:04:23 -0300

    Cambio de colisiones y cambio de imagen de saltar

---
+++ practicos-entregables/entregable3/js/Fondo.js
@@ -0,0 +1,15 @@
+"use strict";
+
+class Fondo{
+    constructor (layer1, layer2, layer3, layer4, layer5, layer6,layer7,constante){
+        this.layer1 = layer1;
+        this.layer2 = layer2;
+        this.layer3 = layer3;
+        this.layer4 = layer4;
+        this.layer5= layer5;
+        this.layer6 = layer6;
+        this.layer7 = layer7;
+        this.constante = constante;
+    }
+
+}
