a9e7b5f61e69a79f5f3c0b2a5c3adcb39bb0bdac
Rosario Tani Gomez <ro.tani.gomez@gmail.com>
2021-11-27 17:45:51 -0300

    Me gusta

--- practicos-entregables/entregable5/js/script-muro.js
+++ practicos-entregables/entregable5/js/script-muro.js
@@ -1,4 +1,7 @@
 
 function pintar (){
-    document.getElementById('box-amigo').style.backgroundColor = rgba(234, 186, 234, 0.6);
+    document.getElementById('box-amigo').style.backgroundColor = "rgba(234, 186, 234, 0.6)";
+}
+function cambiarIcono(){
+    document.getElementById('icono').className ="fa fa-clock-o" ;
 }
